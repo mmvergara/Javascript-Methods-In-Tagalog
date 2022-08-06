47b2407f6b49058e31348e66ea1f2f9db78eeee1
mmvergara <royaleygaming@gmail.com>
2022-08-07 06:38:17 +0800

    releasing in public

---
+++ src/components/AbLinks.js
@@ -0,0 +1,86 @@
+
+const Links = [
+    {
+        id:1,
+        topic:'Array',
+        link:'https://dev.to/mmvergara/javascript-tagalog-arrays-4haj'
+    },
+    {
+        id:2,
+        topic:'Temporal Dead Zone',
+        link:'https://dev.to/mmvergara/javascript-tagalog-temporal-dead-zone-tdz-3344'
+    },
+    {
+        id:3,
+        topic:'Ternary Operator',
+        link:'https://dev.to/mmvergara/javascript-tagalog-ternary-operator-2c0g/'
+    },
+    {
+        id:4,
+        topic:'Optional Chaining',
+        link:'https://dev.to/mmvergara/javascript-tagalog-optional-chaining-464p'
+    },
+    {
+        id:5,
+        topic:'Let Variable',
+        link:'https://dev.to/mmvergara/javascript-tagalog-let-variable-declaration-2l5'
+    },
+    {
+        id:6,
+        topic:'Const Variable',
+        link:'https://dev.to/mmvergara/javascript-tagalog-const-variable-declaration-218m'
+    },
+    {
+        id:7,
+        topic:'Arrow Function',
+        link:'https://dev.to/mmvergara/javascript-tagalog-arrow-function-1gc9'
+    },
+    {
+        id:8,
+        topic:'Nullish Coalescing Operator',
+        link:'https://dev.to/mmvergara/javascript-tagalog-nullish-coalescing-operator-1gkl'
+    },
+    // {
+    //     topic:'',
+    //     link:''
+    // },
+    // {
+    //     topic:'',
+    //     link:''
+    // },
+    // {
+    //     topic:'',
+    //     link:''
+    // },
+    // {
+    //     topic:'',
+    //     link:''
+    // },
+    // {
+    //     topic:'',
+    //     link:''
+    // },
+    // {
+    //     topic:'',
+    //     link:''
+    // },
+    // {
+    //     topic:'',
+    //     link:''
+    // },
+    // {
+    //     topic:'',
+    //     link:''
+    // },
+    // {
+    //     topic:'',
+    //     link:''
+    // },
+    // {
+    //     topic:'',
+    //     link:''
+    // },
+
+    
+]
+export default Links;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="footerContainer">
+            <div>© Javascript-In-Tagalog All Rights Reserved</div>
+            <div>Author: <a href="https://github.com/mmvergara">Vergara, Mark Matthew</a></div>
+        </footer>
+    );
+  }
+  
