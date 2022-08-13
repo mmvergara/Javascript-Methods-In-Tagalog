c12ee27b8616578b6994d9fa9b333b199707c022
mmvergara <royaleygaming@gmail.com>
2022-08-13 14:04:03 +0800

    updated readme and added break statement

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # Javascript-in-tagalog 🌟
 Collection of Learning Resources for Bits and Pieces of Javascript written in Tagalog
-Made By: Vergara Mark Matthew
+Startedby: Vergara Mark Matthew
 
 # Website Link 🌟
 https://Javascript-In-Tagalog.netlify.app/
+
+Aug 13, 2022
+- Added Break Statement 
+
+Aug 6, 2022
+- Added Nullish Coalescing Operator
+
+Aug 5, 2022
+- Added Arrow Function
+- Added Const Variable Declaration
+- Added Let Variable
+- Added Optional Chaining
+- Added Ternary Operator
+
+Aug 4, 2022
+- Added Temporal Dead Zone (TDZ)
+- Added Arrays
+
--- src/Links-JS.js
+++ src/Links-JS.js
@@ -2,84 +2,57 @@ const Links = [
     {
         id:1,
         topic:'Array',
-        link:'https://dev.to/mmvergara/javascript-tagalog-arrays-4haj'
+        link:'https://dev.to/mmvergara/javascript-tagalog-arrays-4haj',
+        author:'MMVergara'
     },
     {
         id:2,
         topic:'Temporal Dead Zone',
-        link:'https://dev.to/mmvergara/javascript-tagalog-temporal-dead-zone-tdz-3344'
+        link:'https://dev.to/mmvergara/javascript-tagalog-temporal-dead-zone-tdz-3344',
+        author:'MMVergara'
     },
     {
         id:3,
         topic:'Ternary Operator',
-        link:'https://dev.to/mmvergara/javascript-tagalog-ternary-operator-2c0g/'
+        link:'https://dev.to/mmvergara/javascript-tagalog-ternary-operator-2c0g/',
+        author:'MMVergara'
     },
     {
         id:4,
         topic:'Optional Chaining',
-        link:'https://dev.to/mmvergara/javascript-tagalog-optional-chaining-464p'
+        link:'https://dev.to/mmvergara/javascript-tagalog-optional-chaining-464p',
+        author:'MMVergara'
     },
     {
         id:5,
         topic:'Let Variable',
-        link:'https://dev.to/mmvergara/javascript-tagalog-let-variable-declaration-2l5'
+        link:'https://dev.to/mmvergara/javascript-tagalog-let-variable-declaration-2l5',
+        author:'MMVergara'
     },
     {
         id:6,
         topic:'Const Variable',
-        link:'https://dev.to/mmvergara/javascript-tagalog-const-variable-declaration-218m'
+        link:'https://dev.to/mmvergara/javascript-tagalog-const-variable-declaration-218m',
+        author:'MMVergara'
     },
     {
         id:7,
         topic:'Arrow Function',
-        link:'https://dev.to/mmvergara/javascript-tagalog-arrow-function-1gc9'
+        link:'https://dev.to/mmvergara/javascript-tagalog-arrow-function-1gc9',
+        author:'MMVergara'
     },
     {
         id:8,
         topic:'Nullish Coalescing Operator',
-        link:'https://dev.to/mmvergara/javascript-tagalog-nullish-coalescing-operator-1gkl'
+        link:'https://dev.to/mmvergara/javascript-tagalog-nullish-coalescing-operator-1gkl',
+        author:'MMVergara'
     },
-    // {
-    //     topic:'',
-    //     link:''
-    // },
-    // {
-    //     topic:'',
-    //     link:''
-    // },
-    // {
-    //     topic:'',
-    //     link:''
-    // },
-    // {
-    //     topic:'',
-    //     link:''
-    // },
-    // {
-    //     topic:'',
-    //     link:''
-    // },
-    // {
-    //     topic:'',
-    //     link:''
-    // },
-    // {
-    //     topic:'',
-    //     link:''
-    // },
-    // {
-    //     topic:'',
-    //     link:''
-    // },
-    // {
-    //     topic:'',
-    //     link:''
-    // },
-    // {
-    //     topic:'',
-    //     link:''
-    // },
-
+    {
+        id:8,
+        topic:'Break Statement',
+        link:'https://dev.to/mmvergara/javascript-tagalog-break-statement-1ghc',
+        author:'MMVergara'
+    },
     
 ]
 export default Links;
