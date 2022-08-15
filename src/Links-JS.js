1f552adb98ff51df33b32eb54069d81c98586cf5
mmvergara <royaleygaming@gmail.com>
2022-08-15 08:31:29 +0800

    updated Links files

--- src/Links-JS.js
+++ src/Links-JS.js
@@ -1,79 +1,78 @@
 const Links = [
     {
-        id:1,
+        id:'array-1',
         topic:'Array',
         link:'https://dev.to/mmvergara/javascript-tagalog-arrays-4haj',
-        author:'MMVergara'
+        author:'MMVergara',
     },
     {
-        id:2,
+        id:'tdz-1',
         topic:'Temporal Dead Zone',
         link:'https://dev.to/mmvergara/javascript-tagalog-temporal-dead-zone-tdz-3344',
-        author:'MMVergara'
+        author:'MMVergara',
     },
     {
-        id:3,
+        id:'ternary-operator-1',
         topic:'Ternary Operator',
         link:'https://dev.to/mmvergara/javascript-tagalog-ternary-operator-2c0g/',
-        author:'MMVergara'
+        author:'MMVergara',
     },
     {
-        id:4,
+        id:'optiona-chaining-1',
         topic:'Optional Chaining',
         link:'https://dev.to/mmvergara/javascript-tagalog-optional-chaining-464p',
-        author:'MMVergara'
+        author:'MMVergara',
     },
     {
-        id:5,
+        id:'let-1',
         topic:'Let Variable',
         link:'https://dev.to/mmvergara/javascript-tagalog-let-variable-declaration-2l5',
-        author:'MMVergara'
+        author:'MMVergara',
     },
     {
-        id:6,
+        id:'const-1',
         topic:'Const Variable',
         link:'https://dev.to/mmvergara/javascript-tagalog-const-variable-declaration-218m',
-        author:'MMVergara'
+        author:'MMVergara',
     },
     {
-        id:7,
+        id:'arrow-fn-1',
         topic:'Arrow Function',
         link:'https://dev.to/mmvergara/javascript-tagalog-arrow-function-1gc9',
-        author:'MMVergara'
+        author:'MMVergara',
     },
     {
-        id:8,
+        id:'nco-1',
         topic:'Nullish Coalescing Operator',
         link:'https://dev.to/mmvergara/javascript-tagalog-nullish-coalescing-operator-1gkl',
-        author:'MMVergara'
+        author:'MMVergara',
     },
     {
-        id:9,
+        id:'break-1',
         topic:'Break Statement',
         link:'https://dev.to/mmvergara/javascript-tagalog-break-statement-1ghc',
-        author:'MMVergara'
+        author:'MMVergara',
     },
-    
     {
-		id:10,
+		id:'undefined-1',
         topic:"Undefined",
         link:"https://dev.to/mmvergara/javascript-tagalog-undefined-44ic",
         author:"MMVergara",
     },
     {
-		id:11,
+		id:'null-1',
         topic:"Null",
         link:"https://dev.to/mmvergara/javascript-tagalog-null-4mo3",
         author:"MMVergara",
     },
     {
-		id:12,
+		id:'boolean-1',
         topic:"Boolean",
         link:"https://dev.to/mmvergara/javascript-tagalog-boolean-4i5k",
         author:"MMVergara",
     },
     {
-		id:13,
+		id:'strings-1',
         topic:"Strings",
         link:"https://dev.to/mmvergara/javascript-tagalog-strings-2ee0",
         author:"MMVergara",
@@ -88,7 +87,19 @@ const Links = [
 		id:'forof-1',
         topic:"",
         link:"https://dev.to/mmvergara/javascript-tagalog-for-of-loop-1898",
-        author:"MMVergara"
+        author:"MMVergara",
+    },
+    {	
+		id:'equality-1',
+        topic:"Equality",
+        link:"https://dev.to/mmvergara/javascript-tagalog-equality-strict-equality-2med",
+        author:"MMVergara",
+    },
+    {	
+		id:'strict-equality-1',
+        topic:"Strict Equality",
+        link:"https://dev.to/mmvergara/javascript-tagalog-equality-strict-equality-2med",
+        author:"MMVergara",
     },
     
 ]
