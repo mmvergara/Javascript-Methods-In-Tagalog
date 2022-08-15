88d43867781d11e54df42f2590143c55d1e78814
mmvergara <royaleygaming@gmail.com>
2022-08-15 08:41:17 +0800

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,7 @@ export default function Footer() {
         <footer className="footerContainer">
             <div>© 2022 Javascript-In-Tagalog All Rights Reserved</div>
             <div>StartedBy: <a href="https://github.com/mmvergara">Vergara, Mark Matthew</a></div>
+            <div>Creation Date: August 7, 2022</div>
         </footer>
     );
   }
